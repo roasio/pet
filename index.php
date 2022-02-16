728cbfd9f95e75626f484b1b27e5f455ada6860d
Guilherme Roasio <guilhermeroasio.dev@gmail.com>
2022-02-16 17:31:33 -0300

    teste commit

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	$db = open_database(); 
 	
 	if ($db) {
-		echo '<h1>Banco de Dados Conectado!</h1>';
+		echo '<h1>Banco de Dados Conectado!!</h1>';
 	} else {
-		echo '<h1>ERRO: Não foi possível Conectar!</h1>';
+		echo '<h1>ERRO: Não foi possível Conectar!!</h1>';
 	}
 ?>
